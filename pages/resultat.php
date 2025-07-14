4bc8b7d025f219a158b046edfe5c1f3c081f4ac8
MialyNyanj <nyanjatiana@gmail.com>
2025-07-14 14:40:44 +0300

    modif resultat display

--- pages/resultat.php
+++ pages/resultat.php
@@ -1,5 +1,3 @@
-
-
 <?php
 include('../inc/fonctions.php');
 session_start();
@@ -7,7 +5,7 @@ session_start();
 $cat = $_GET['cat'];
 $nom = $_GET['nom'];
 $av = $_GET['available'] ?? null;
-$categories = getCategories();
+$categorie = getCategorieObjet();
 $object = searchObjet($cat, $nom, $av);
 ?>
 
@@ -18,7 +16,7 @@ $object = searchObjet($cat, $nom, $av);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
-     <link rel="stylesheet" type="text/css" href="../assets/css/style2.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/style2.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
     <script src="../assets/js/bootstrap.bundle.min.js"></script>
@@ -34,27 +32,27 @@ $object = searchObjet($cat, $nom, $av);
     <main class="container">
         <section class="title">
             <h2>Résultats de recherche</h2>
-            <p>Catégorie : <?= htmlspecialchars($cat) ?>, Nom : "<?= htmlspecialchars($nom) ?>"", Disponible : <?= $av ? 'Oui' : 'Non' ?></p>
+            <p>Catégorie : <?= htmlspecialchars($categorie[$cat]['nom_categorie']) ?>, Nom : "<?= htmlspecialchars($nom) ?>", Disponible : <?= $av ? 'Oui' : 'Non' ?></p>
         </section>
 
         <section class="property">
             <?php foreach ($object as $o): ?>
-                    <article class="card" style="width: 18rem;">
-                        <div>
-                            <img src="../assets/img/<?= htmlspecialchars($o['nom_image']); ?>" class="card-img-top" alt="...">
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title"><strong><?= htmlspecialchars($o['nom_objet']); ?></strong></h5>
-                            <p class="card-text"><?= htmlspecialchars($o['nom_membre']) ?></p>
-                        </div>
-                        <ul class="list-group list-group-flush">
+                <article class="card" style="width: 18rem;">
+                    <div>
+                        <img src="../assets/img/<?= htmlspecialchars($o['nom_image']); ?>" class="card-img-top" alt="...">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title"><strong><?= htmlspecialchars($o['nom_objet']); ?></strong></h5>
+                        <p class="card-text"><?= htmlspecialchars($o['nom_membre']) ?></p>
+                    </div>
+                    <ul class="list-group list-group-flush">
 
-                        </ul>
-                        <div class="hstack gap-3">
-                            <h6 class="p-3"><strong><?= htmlspecialchars($o['nom_categorie']) ?></strong></h6>
-                        </div>
-                    </article>
-            
+                    </ul>
+                    <div class="hstack gap-3">
+                        <h6 class="p-3"><strong><?= htmlspecialchars($o['nom_categorie']) ?></strong></h6>
+                    </div>
+                </article>
+
             <?php endforeach; ?>
         </section>
     </main>
